40c52bce99c3e863e154070e5a1afcf98784f8bc
PIEDLA <luis.piedrahita@iefedericoozanam.edu.co>
2026-03-26 16:46:48 -0500

    Fase 3

--- SportsLeague.API/Program.cs
+++ SportsLeague.API/Program.cs
@@ -19,13 +19,23 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 
 builder.Services.AddScoped<ITeamRepository, TeamRepository>();
 
-builder.Services.AddScoped<IPlayerRepository, PlayerRepository>(); // NUEVO
+builder.Services.AddScoped<IPlayerRepository, PlayerRepository>();
+
+builder.Services.AddScoped<IRefereeRepository, RefereeRepository>(); // NUEVO
+
+builder.Services.AddScoped<ITournamentRepository, TournamentRepository>(); // NUEVO
+
+builder.Services.AddScoped<ITournamentTeamRepository, TournamentTeamRepository>(); // NUEVO
 
 // ── Services ──
 
 builder.Services.AddScoped<ITeamService, TeamService>();
 
-builder.Services.AddScoped<IPlayerService, PlayerService>(); // NUEVO
+builder.Services.AddScoped<IPlayerService, PlayerService>();
+
+builder.Services.AddScoped<IRefereeService, RefereeService>(); // NUEVO
+
+builder.Services.AddScoped<ITournamentService, TournamentService>(); // NUEVO
 
 // ── AutoMapper ──
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
