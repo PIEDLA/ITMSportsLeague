59de5ff2fb7fbcad2b39851edeb35f6ce4c3d532
PIEDLA <luis.piedrahita@iefedericoozanam.edu.co>
2026-04-02 23:03:08 -0500

    Creation of entitys Sponsor, Tournament and entity type N:M TournamentSponsor

---
+++ SportsLeague.Domain/Entities/Sponsor.cs
@@ -0,0 +1,22 @@
+﻿using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.Domain.Entities
+{
+    public class Sponsor : AuditBase
+    {
+
+        public string Name { get; set; } = string.Empty;
+
+        public string ContactEmail { get; set; } = string.Empty;
+
+        public string Phone { get; set; } = string.Empty;
+        public string WebsiteUrl { get; set; } = string.Empty;
+
+        public int Category { get; set; }
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation Properties
+
+        public ICollection<TournamentSponsor> TournamentSponsors { get; set; } = new List<TournamentSponsor>();
+    }
+}
